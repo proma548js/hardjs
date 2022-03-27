5c0e9ec6560d897772b1a3c4f5f2c35ead16dca4
Evgeniu <traingloo24@gmail.com>
2022-03-27 11:30:11 +0300

    измененный название переменных

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const a = 266219;
-const b = a.toString().split('');
-const c = b.reduce((acc, rec) => acc * rec);
-const d = c ** 3;
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(typeof (d));
-console.log(String(d).slice(0, 2));
+const num = 266219;
+const AddArray = num.toString().split('');
+const multiplication = AddArray.reduce((acc, rec) => acc * rec);
+const cubed = multiplication ** 3;
+console.log(AddArray);
+console.log(multiplication);
+console.log(cubed);
+console.log(typeof (cubed));
+console.log(String(cubed).slice(0, 2));
